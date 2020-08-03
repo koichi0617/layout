d1f49db45f07d88db530dd671b017c1a92486e49
koichi0617 <koichi.0617@i.softbank.jp>
2020-08-03 11:35:58 +0900

    rails tutorial ~5

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,11 @@
+module ApplicationHelper
+  #ページごとの完全なタイトルを返す
+  def full_title(page_titile = '')
+    base_title = 'Ruby on Rails Tutorial Sample App'
+    if page_titile.empty?
+      base_title
+    else
+      page_titile + " | " + base_title
+    end
+  end
+end
